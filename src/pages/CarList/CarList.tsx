9aa819d3a906b9e9e6dd4d6d9dde43efa376886e
Huzeyfe ÖZVEREN <huzeyfeozverenn@gmail.com>
2024-01-26 09:11:04 +0300

    Added Sliders

--- src/pages/CarList/CarList.tsx
+++ src/pages/CarList/CarList.tsx
@@ -2,37 +2,37 @@ import { useEffect, useState } from "react";
 import CarService from "../../services/carService";
 import CarCard from "../../components/layout/CarCard/CarCard";
 import { GetByIdCarResponse } from "../../models/car/responses/GetByIdCarResponse";
+import { GetAllCarsResponse } from "../../models/car/responses/GetAllCarsResponse";
+import carService from "../../services/carService";
 
 type Props = {};
 
 const CarList = (props: Props) => {
-  const [cars, setCars] = useState<GetByIdCarResponse[]>([]);
+  const [cars, setCars] = useState<GetAllCarsResponse[]>([]);
 
   useEffect(() => {
     fetchCars();
   }, []);
 
-  const onCarDelete = (id: number) => {
-    setCars(cars.filter(i => i.id !== id));
-  };
+
 
   const fetchCars = () => {
     CarService.getAll().then((response: any) => {
-      setCars(response.data.cars);
+      setCars(response.data.data);
     });
   };
-
+  console.log(cars)
   return (
-    <div className="container mt-5">
-      <div className="row">
-        {cars.map(car => (
-          <div key={car.id} className="col-lg-3 col-md-6 col-12 mb-5">
-            <CarCard onDelete={onCarDelete} car={car} />
-          </div>
-        ))}
-      </div>
+    <div className="container">
+       <div className="row">
+                {cars.map(car => (
+                    <div key={car.id} className="col-lg-3 col-md-6 col-12 mb-5 card my-3">
+                        <CarCard car={car} />
+                    </div>
+                ))}
+            </div>
     </div>
-  );
+);
 };
 
 export default CarList;
