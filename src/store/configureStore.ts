6ca09641530eaffedf7f852e4b83763f0b348a03
Bedirhan Balcı <bedirhanbalci@outlook.com>
2024-02-03 12:46:02 +0300

    Added Auth Store and Login Processes

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,1 +1,15 @@
-export {};
+import { storeAuthState } from "./authStorage";
+import { authReducer } from "./slices/authSlice";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+export const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
+
+store.subscribe(() => {
+  storeAuthState(store.getState().auth);
+});
